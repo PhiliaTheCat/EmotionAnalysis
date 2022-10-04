b096e4e98b6033090dd264751049f1f675a5d486
PhiliaTheCat <2787974051@qq.com>
2022-10-04 14:54:41 +0800

    Partition Optimized

--- Process.py
+++ Process.py
@@ -2,23 +2,26 @@ from encodings import utf_8
 from enum import Flag
 import jieba 
 
-def is_legal(sample: str):
+def is_legal(sample: str): # symbol is not legal
     if sample == "\n":
         return False
-    if sample == "，":
+    if (sample == "、"):
+        return False
+    if sample == "，" or sample == ",":
         return False
     if sample == "。":
         return False 
-    if (sample == "！"):
+    if (sample == "！" or sample == "？"):
+        return False
+    if (sample == "…"):
         return False
     return True
 
-
 # open files
-fin = open("./外卖评论.csv", "r", encoding = "utf-8")
-fout = open("./Partition.txt", "w", encoding = "utf-8")
+fin = open(".\\外卖评论.csv", "r", encoding = "utf-8")
+fout = open(".\\Partition.txt", "w", encoding = "utf-8")
 
-fin.seek(14, 0)
+fin.seek(14, 0) # jump over example
 
 for each_line in fin:
     cache = each_line.partition(",")
